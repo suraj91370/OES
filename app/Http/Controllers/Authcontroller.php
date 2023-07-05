82b730c5addf363c3517d822ea12d81b2cf31b47
Abhishek <abhishekdilipyadav@gmail.com>
2023-07-05 16:15:48 +0530

    First commit

---
+++ app/Http/Controllers/Authcontroller.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class Authcontroller extends Controller
+{
+    //
+    public function loadRegister()
+    {
+        return view('register');
+    }
+    public function studentRegister(Request $request)
+    {
+        $request->validate([
+            'name' => 'string|required|min:2',
+            'email' => 'string|email|required|max:100|unique:users',
+            'password' => 'string|required|confirmed|min:6'
+        ]);
+
+        $user = new user;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return back()->with('success','your Registration has been successful.');
+
+
+    }
+}
